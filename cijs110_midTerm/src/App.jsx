cf28bb548f96a4b3eb73f444e62a66dd76a385b6
Sloth-nvbach <SienneSloth@outlook.com>
2026-03-20 13:59:14 +0700

    More css fix (still got bug at Carousel-Header-lead)

--- cijs110_midTerm/src/App.jsx
+++ cijs110_midTerm/src/App.jsx
@@ -7,16 +7,19 @@ import Card from './components/Card/Card';
 import { moviesData } from "../src/data/data.js";;
 
 function App() {
-  const featuedMovie = moviesData.find(movie => movie.id === "1");
-  const lastestRelease = moviesData.filter(movie => movie.id !== "1");
-  const bannerImage = featuedMovie ? featuedMovie.image.replace("../assets/images/", "/images/") : "";
+  const featuredMovie = moviesData.find(movie => movie.id === "1");
+  const latestRelease = moviesData.filter(movie => movie.id !== "1");
+  const bannerImage = featuredMovie ? featuredMovie.image.replace("../assets/images/", "/images/") : "";
 
   return (
     <div style={{
       backgroundColor: "#192026",
       minHeight: "100vh",
       fontFamily: "Poppins, sans-serif",
-      padding: "15px 83px"
+      padding: "15px 83px",
+      maxWidth: "1340px",
+      margin: "0 auto",
+      boxSizing: "border-box"
     }}>
 
       {/* Navbar */}
@@ -28,11 +31,12 @@ function App() {
       <Text color="#FFFFFF" fontSize="32px" fontWeight="600" style={{ lineHeight: "112.5%" }}>Explore</Text>
       <Text color="#868686" fontSize="22px" fontWeight="400" style={{ lineHeight: "112.5%", marginTop: "8px" }}>What are you gonna watch today ?</Text>
 
-      {featuedMovie && (
+      {featuredMovie && (
         <Carousel
           image={bannerImage}
-          movieName={featuedMovie.movieName}
-          description={featuedMovie.description} />
+          movieName={featuredMovie.movieName}
+          description={featuredMovie.description}
+        />
       )}
 
 
@@ -54,7 +58,7 @@ function App() {
           width: "100%",
           flexWrap: "wrap"
         }}>
-          {lastestRelease.map((movie) => {
+          {latestRelease .map((movie) => {
             const cleanImagePath = movie.image.replace("../assets/images/", "/images/");
 
             return (
